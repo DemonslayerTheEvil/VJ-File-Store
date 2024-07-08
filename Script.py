b3f562074d6f0474374f887c47d9bcb6a76085fd
DemonslayerTheEvil <130355084+DemonslayerTheEvil@users.noreply.github.com>
2024-07-08 08:59:53 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,12 +10,12 @@ class script(object):
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-    
-     CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_caption}
+
+  CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
 sɪᴢᴇ ⚙️: {file_size}
 
-Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/LDC_OFFICIAL)</b>""" 
+Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/vj_botz)</b>"""
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
