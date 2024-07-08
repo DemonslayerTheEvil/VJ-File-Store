70ef8c1f72ff3abf7dd665c3b92f137fe56a8c18
DemonslayerTheEvil <130355084+DemonslayerTheEvil@users.noreply.github.com>
2024-07-08 08:43:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,7 +15,7 @@ class script(object):
 
 sɪᴢᴇ ⚙️: {file_size}</a></b>"""
 
-<b>Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/LDC_OFFICIAL)</b> 
+"""<b>Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/LDC_OFFICIAL)</b>"""
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
